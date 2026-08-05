f119968d0afb24a56a7b4d83edfaed79f64f9c47
Keshav <ishu54321kumar@gmail.com>
2026-08-05 23:51:50 +0530

    d:\VS code\javascript_practice\jsExample

---
+++ 03_JS/javascript_practice/javascript_practice/jsExample/index.js
@@ -0,0 +1,18 @@
+let selecting = document.querySelector('#title');
+let button = document.querySelector('#button');
+let textchange = document.querySelector('.text');
+let anotherway = document.querySelector('.text');
+
+// anotherway.forEach((num)=>console.log(num))
+// anotherway.map((num)=>
+//     button.addEventListener("click",function(){
+//         selecting.textContent="i am keshav";
+//     })
+// ) 
+
+let list = document.querySelectorAll('li');
+console.log(list)
+
+list.forEach(()=>{
+    
+})
